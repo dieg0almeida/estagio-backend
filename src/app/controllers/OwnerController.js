f2fce650ce4d7e5dc7316542610a85081fca2e3c
bittr <atchim.gama.silva@gmail.com>
2020-05-12 19:56:09 -0300

    Implemented full owner info in owner's show method.

--- src/app/controllers/OwnerController.js
+++ src/app/controllers/OwnerController.js
@@ -8,9 +8,38 @@ module.exports = {
     },
     async show(req, res){
         const results = await Owner.findById(req.params.id);
+        const raw_info = results[0][0];
+        const owner_info = {
+            owner: {},
+            add_family_info: {},
+            family_health_info: {},
+            family_info: {},
+            owners_partners: {},
+            property_info: {},
+        };
 
-        const owner = results[0][0];
-        return res.json({ owner });
+        for (let name of Object.getOwnPropertyNames(raw_info)) {
+            if (name.startsWith('owner_')) {
+                owner_info.owner[name.replace('owner_', '')] = raw_info[name];
+            }
+            else if (name.startsWith('add_family_info_')) {
+                owner_info.add_family_info[name.replace('add_family_info_', '')] = raw_info[name];
+            }
+            else if (name.startsWith('family_health_info_')) {
+                owner_info.family_health_info[name.replace('family_health_info_', '')] = raw_info[name];
+            }
+            else if (name.startsWith('family_info_')) {
+                owner_info.family_info[name.replace('family_info_', '')] = raw_info[name];
+            }
+            else if (name.startsWith('owners_partners_')) {
+                owner_info.owners_partners[name.replace('owners_partners_', '')] = raw_info[name];
+            }
+            else if (name.startsWith('property_info_')) {
+                owner_info.property_info[name.replace('property_info_', '')] = raw_info[name];
+            }
+        }
+
+        return res.json({ owner_info });
     },
     async post(req, res){
         await Owner.create(req.body);
