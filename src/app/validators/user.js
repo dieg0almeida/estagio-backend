ccfc19387bd6754e1f1da9c3cedb871bf2993fc8
Weri Oliveira <werioliveira@hotmail.com>
2020-05-22 13:18:54 -0300

    Revert "Reverting some mess that I did."
    This reverts commit 8a244d15a67460bbcc3130916507b4628b80acb8.

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -4,16 +4,16 @@ module.exports = {
         const { email, password, is_admin } = req.body;
 
         if (!email) {
-            return res.json({ erro: "Email can't be blank" });
+            return res.json({ erro: "Email não pode ser vazio" });
         }
         if (!password) {
-            return res.json({ erro: "Password can't be blank" });
+            return res.json({ erro: "Senha não pode ser vazia" });
         }
         if (!is_admin) {
-            return res.json({ erro: "Select if user is admin or not" });
+            return res.json({ erro: "Selecione se o usuário é administrador ou não" });
         }
         if (is_admin != true || is_admin != false || is_admin != 0 || is_admin != 1) {
-            return res.json({ erro: "Field Admin is invalid" });
+            return res.json({ erro: "Campo Admin é inválido" });
         }
 
         next();
